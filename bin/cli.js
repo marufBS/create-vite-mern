d7af5b8c52dcb1ee26020198a9c7cd0360aafc45
Udayan Basak <thisisudayan@gmail.com>
2024-08-08 00:39:51 +0600

    bush

--- bin/cli.js
+++ bin/cli.js
@@ -1,26 +1,44 @@
-const {execSync} = require('child_process')
+#!/usr/bin/env node
 
-const runCommand = command => {
-    try {
-        execSync(`${command}`,{stdio:'inherit'})
-    } catch (e) {
-        console.error(`Failed to execute ${command}`, e)
-        return false
+const { execSync } = require('child_process');
+const fs = require('fs');
+const path = require('path');
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+const args = process.argv.slice(2);
+
+function createProject(projectName) {
+    const projectPath = path.join(process.cwd(), projectName);
+
+    if (fs.existsSync(projectPath)) {
+        console.error(`Directory ${projectName} already exists.`);
+        process.exit(1);
     }
-    return true
+
+    // Create the project directory
+    fs.mkdirSync(projectPath);
+
+    // Initialize the project (this example uses Vite for a React project)
+    console.log(`Creating a new Vite project in ${projectPath}`);
+    execSync(`npm create vite@latest ${projectName} --template react`, { stdio: 'inherit' });
+
+    console.log('Project setup complete.');
 }
 
-const repoName = process.argv[2]
-const gitCheckoutCommand = `git clone --depth 1 https://github.com/marufBS/create-vite-mern ${repoName}`
-const installDepsCommand = `cd ${repoName} && npm install`
-
-console.log(`Creating project with name ${repoName}`)
-const checkOut = runCommand(gitCheckoutCommand)
-if(!checkOut) process.exit(-1)
-
-console.log(`Installing dependencies for ${repoName}`)
-const installedDeps = runCommand(installDepsCommand)
-if(!installedDeps) process.exit(-1)
-
-console.log("Congratulations! You are ready to Deploy")
-console.log(`cd ${repoName} && npm run dev`)
+if (args.length < 1) {
+    rl.question('Please provide a project name: ', (projectName) => {
+        if (!projectName) {
+            console.error('Project name is required.');
+            process.exit(1);
+        }
+        createProject(projectName);
+        rl.close();
+    });
+} else {
+    createProject(args[0]);
+}
