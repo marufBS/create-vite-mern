191b7acd9b2601e57d244c0a817573feb1eb8bb7
marufBS <0909maruf@gmail.com>
2024-08-08 21:48:00 +0600

    project creation done

--- bin/cli.js
+++ bin/cli.js
@@ -1,13 +1,49 @@
 #!/usr/bin/env node
-const {program} = require("commander")
-// import { program } from "commander";
 
-program
-  .version("1.0.0")
-  .description("My Node CLI")
-  .option("-n, --name <type>", "Add your name")
-  .action((options) => {
-    console.log(`Hey, ${options.name}!`);
-  });
+const { execSync } = require('child_process');
+const fs = require('fs');
+const path = require('path');
+const readline = require('readline');
 
-program.parse(process.argv);
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+const args = process.argv.slice(2);
+
+function fetchRepo() {
+    
+}
+
+
+
+function createProject(projectName) {
+    const projectPath = path.join(process.cwd(), projectName);
+
+    if (fs.existsSync(projectPath)) {
+        console.error(`Directory ${projectName} already exists.`);
+        process.exit(1);
+        // Create the project directory
+    } else {
+        fs.mkdirSync(projectPath);
+        console.log('Project setup complete.');
+        fetchRepo()
+        process.exit(1)
+    }
+
+
+
+}
+if (args.length < 1) {
+    rl.question('Please provide a project name: ', (projectName) => {
+        if (!projectName) {
+            console.error('Project name is required.');
+            process.exit(1);
+        }
+        createProject(projectName);
+        rl.close();
+    });
+} else {
+    createProject(args[0]);
+}
